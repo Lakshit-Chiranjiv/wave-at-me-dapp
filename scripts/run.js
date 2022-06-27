e9fe6f5b1e6ae5f7c9870a087c52167f82c4c917
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-27 21:56:21 +0530

    code for other address to wave

--- scripts/run.js
+++ scripts/run.js
@@ -15,6 +15,11 @@ const main = async() => {
     await waveTxn.wait()
 
     waveCount = await waveContract.getTotalWaves()
+
+    waveTxn = await waveContract.connect(somePerson).wave();
+    await waveTxn.wait();
+    
+    waveCount = await waveContract.getTotalWaves();
 }
 
 const runMain = async() => {
