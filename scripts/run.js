17295dd444c0c58928a6bd70176f6817fc52c824
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-27 21:30:13 +0530

    func to run main func with try catch

--- scripts/run.js
+++ scripts/run.js
@@ -7,3 +7,14 @@ const main = async() => {
     console.log(`Wave Portal contract deployed to ${waveContract.address}`)
 }
 
+const runMain = async() => {
+    try {
+        await main()
+        process.exit(0)
+    } catch (error) {
+        console.log("Some error occured : "+error)
+        process.exit(1)
+    }
+}
+
+runMain()
