52b6cf90ba09d397972cedfec546f9e468864e8d
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-27 21:51:44 +0530

    called wave and gettotalwaves func in script

--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,20 @@
 
 const main = async() => {
+    const [owner, somePerson] = await hre.ethers.getSigners()
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal")
     const waveContract = await waveContractFactory.deploy()
     await waveContract.deployed()
     console.log(`Wave Portal contract deployed to ${waveContract.address}`)
+    console.log(`the contract deployed by ${owner.address}`)
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves()
+    
+    let waveTxn;
+    waveTxn = await waveContract.wave()
+    await waveTxn.wait()
+
+    waveCount = await waveContract.getTotalWaves()
 }
 
 const runMain = async() => {
