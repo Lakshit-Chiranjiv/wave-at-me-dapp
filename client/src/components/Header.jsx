421c1d2916a1e6c0e2dc1d526f283f917f9218a1
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-28 14:58:49 +0530

    extracted header code

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="pure-u-1 heading">
+        <h1>Wave At ME Portal</h1>
+    </div>
+  )
+}
+
+export default Header
