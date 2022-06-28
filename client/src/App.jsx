0713bdd0497ceaa2dbb6f0dcc6de22f2105ff6ff
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-28 20:15:48 +0530

    code to check for wallet provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './../node_modules/purecss/build/pure.css'
 import './App.css'
 import Header from './components/Header'
@@ -6,6 +6,28 @@ import Nav from './components/Nav'
 import WaveInput from './components/WaveInput'
 
 function App() {
+
+
+  const checkWalletConnection = async () => {
+    try {
+      const { ethereum } = window
+
+      if(ethereum){
+        console.log("ethereum object exists : ",ethereum)
+      }
+      else{
+        console.log("ethereum object doesn't exists, No wallet found")
+      }
+
+
+    } catch (error) {
+      console.log("Message : "+error)
+    }
+  }
+
+  useEffect(()=>{
+    checkWalletConnection()
+  },[])
 
   return (
     <div className="pure-g App">
