49988b8c58fd56aa57848ea91f943dd6c1c1d358
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-28 15:00:01 +0530

    extracted nav code

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className="pure-u-1-2 pure-menu pure-menu-horizontal nav">
+        <ul className="pure-menu-list">
+        
+        
+        <li className="pure-menu-item">
+            <a href="#" className="pure-menu-link"><h3>See All Waves</h3></a>
+        </li>
+        <li className="pure-menu-item">
+            <a href="#" className="pure-menu-link"><h3>Lets Wave</h3></a>
+        </li>
+        <li className="pure-menu-item">
+            <a href="#" className="pure-menu-link"><h3>About us</h3></a>
+        </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Nav
