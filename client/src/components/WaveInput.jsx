b9411890d60aa35cb5399dae368bb86aaa54d47a
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-28 19:29:29 +0530

    extracted wave input code

---
+++ client/src/components/WaveInput.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const WaveInput = () => {
+  return (
+    <div className='pure-u-1 waveDiv'>
+        <label htmlFor="waveMsg" className='myText'>Enter your Wave Message</label><br />
+        <textarea name="waveMsg" className='myText' cols="30" rows="10" placeholder='Waving Hello from Far Away!!'></textarea>
+        <button className='pure-button'>Wave your Message 👋</button>
+    </div>
+  )
+}
+
+export default WaveInput
