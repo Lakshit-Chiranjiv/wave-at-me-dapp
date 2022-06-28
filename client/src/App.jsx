f5529e37d9df9cc23732eeadbf7da0f2c951c6b1
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-28 15:00:10 +0530

    used Nav component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,27 +2,14 @@ import { useState } from 'react'
 import './../node_modules/purecss/build/pure.css'
 import './App.css'
 import Header from './components/Header'
+import Nav from './components/Nav'
 
 function App() {
 
   return (
     <div className="pure-g App">
       <Header/>
-      <div className="pure-u-1-2 pure-menu pure-menu-horizontal nav">
-        <ul className="pure-menu-list">
-        
-        
-          <li className="pure-menu-item">
-            <a href="#" className="pure-menu-link"><h3>See All Waves</h3></a>
-          </li>
-          <li className="pure-menu-item">
-            <a href="#" className="pure-menu-link"><h3>Lets Wave</h3></a>
-          </li>
-          <li className="pure-menu-item">
-            <a href="#" className="pure-menu-link"><h3>About us</h3></a>
-          </li>
-        </ul>
-      </div>
+      <Nav/>
     </div>
   )
 }
