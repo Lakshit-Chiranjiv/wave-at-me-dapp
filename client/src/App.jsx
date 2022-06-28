630b5fe4b2ff0bea60e5dfd5dda761ed6cecf903
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-28 19:28:15 +0530

    wave Div code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,11 @@ function App() {
     <div className="pure-g App">
       <Header/>
       <Nav/>
+      <div className='pure-u-1 waveDiv'>
+        <label htmlFor="waveMsg" className='myText'>Enter your Wave Message</label><br />
+        <textarea name="waveMsg" className='myText' cols="30" rows="10" placeholder='Waving Hello from Far Away!!'></textarea>
+        <button className='pure-button'>Wave your Message 👋</button>
+      </div>
     </div>
   )
 }
