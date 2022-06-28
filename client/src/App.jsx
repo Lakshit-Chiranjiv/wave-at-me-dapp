c84cd67e2aa396ad2c0729c265dc15697f3dbe3b
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-28 19:29:44 +0530

    used wave input component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import './../node_modules/purecss/build/pure.css'
 import './App.css'
 import Header from './components/Header'
 import Nav from './components/Nav'
+import WaveInput from './components/WaveInput'
 
 function App() {
 
@@ -10,11 +11,7 @@ function App() {
     <div className="pure-g App">
       <Header/>
       <Nav/>
-      <div className='pure-u-1 waveDiv'>
-        <label htmlFor="waveMsg" className='myText'>Enter your Wave Message</label><br />
-        <textarea name="waveMsg" className='myText' cols="30" rows="10" placeholder='Waving Hello from Far Away!!'></textarea>
-        <button className='pure-button'>Wave your Message 👋</button>
-      </div>
+      <WaveInput/>
     </div>
   )
 }
