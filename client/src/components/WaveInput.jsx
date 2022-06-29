ac8a4427b90f7765553b541bdd5c0eddde26751e
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-29 15:20:12 +0530

    passed wavemsg & used in onchange textarea

--- client/src/components/WaveInput.jsx
+++ client/src/components/WaveInput.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const WaveInput = ({waveHandler}) => {
+const WaveInput = ({waveHandler,setWaveMsg}) => {
   return (
     <div className='pure-u-1 waveDiv'>
         <label htmlFor="waveMsg" className='myText'>Enter your Wave Message</label><br />
-        <textarea name="waveMsg" className='myText' cols="30" rows="10" placeholder='Waving Hello from Far Away!!'></textarea>
+        <textarea name="waveMsg" className='myText' cols="30" rows="10" placeholder='Waving Hello from Far Away!!' onChange={(e) => {setWaveMsg(e.target.value)}}></textarea>
         <button className='pure-button' onClick={waveHandler}>Wave your Message 👋</button>
     </div>
   )
