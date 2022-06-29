0e18127474aad0db1edcc917d9ca1bccb19d818b
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-29 16:07:46 +0530

    updated deploy file to fund 0.01 eth to contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,7 +7,9 @@ const main = async() => {
     
 
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal")
-    const waveContract = await waveContractFactory.deploy()
+    const waveContract = await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther("0.01"),
+    })
     await waveContract.deployed()
     console.log(`Wave Portal contract deployed to ${waveContract.address}`)
 }
