b026f8e0971e1788ec89cbc935fef91759e255eb
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-29 21:51:25 +0530

    conditional loader and btn render

--- client/src/components/WaveInput.jsx
+++ client/src/components/WaveInput.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Loader from './Loader'
 
-const WaveInput = ({waveHandler,setWaveMsg}) => {
+const WaveInput = ({waveHandler,setWaveMsg,waveBtnLoading,setWaveBtnLoading}) => {
   return (
     <div className='pure-u-1 waveDiv'>
         <label htmlFor="waveMsg" className='myText'>Enter your Wave Message</label><br />
         <textarea name="waveMsg" className='myText' cols="30" rows="10" placeholder='Waving Hello from Far Away!!' onChange={(e) => {setWaveMsg(e.target.value)}}></textarea>
-        <button className='pure-button myText' onClick={waveHandler}>Wave your Message 👋</button>
+        {
+          waveBtnLoading ? 
+          <Loader/> :
+          <button className='pure-button myText' onClick={waveHandler}>Wave your Message 👋</button>
+        }
     </div>
   )
 }
