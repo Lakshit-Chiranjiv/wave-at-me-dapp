35ac45ccd845de6fc19868f3aaff9bea5fd506d1
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-29 09:21:56 +0530

    passed waveHandler as prop

--- client/src/components/WaveInput.jsx
+++ client/src/components/WaveInput.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const WaveInput = () => {
+const WaveInput = ({waveHandler}) => {
   return (
     <div className='pure-u-1 waveDiv'>
         <label htmlFor="waveMsg" className='myText'>Enter your Wave Message</label><br />
         <textarea name="waveMsg" className='myText' cols="30" rows="10" placeholder='Waving Hello from Far Away!!'></textarea>
-        <button className='pure-button'>Wave your Message 👋</button>
+        <button className='pure-button' onClick={waveHandler}>Wave your Message 👋</button>
     </div>
   )
 }
